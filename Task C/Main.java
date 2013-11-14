04ca0c3894753c564cc10c0659c149cc3e53d0e0
Yura <yurger2009@gmail.com>
2013-11-14 20:41:09 +0400

    Finished

--- Task C/Main.java
+++ Task C/Main.java
@@ -1,35 +1,66 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner sc=new Scanner(System.in);
-		//Scanner sc=new Scanner(new FileInputStream("input.txt"));
+//		Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(new FileInputStream("input.txt"));
+		PrintWriter out = new PrintWriter("output.txt");
 		//a - smaller, b - bigger
-		int h=sc.nextInt(), a=sc.nextInt(),b=sc.nextInt();
+		int h=sc.nextInt(), b=sc.nextInt(),a=sc.nextInt();
 		long sum=0;
+		/**
+		 * Correct
+		 *  _
+		 * | |
+		 * | |_
+		 * |___| 
+		 */
 		for(int i=0;i<=h-b;i++){
 			sum+=(2*i+1);
 		}
-		for(int i=(b-a);i<=h-a;i++){
-			sum+=(2*(i-b/2+1));
+		/**
+		 * correct
+		 *      _
+		 *  ___| |
+		 * |_____|
+		 */
+		int k=2*h+1-a-b;
+		while(k>0){
+			sum+=k;
+			k-=2;
 		}
+		/**
+		 * correct
+		 *  _____
+		 * |  ___| 
+		 * |_|
+		 */
 		if(b%2==1)
 			for(int i=(b-1)/2;i<=h-a;i++)
 				sum+=(2*i+2-b);			
 		else
 			for(int i=b/2;i<=h-a;i++)
-				sum+=(2*(i-b/2+1));	
+				sum+=(2*(i-b/2+1));
+		/**
+		 *  Correct
+		 *  ___
+		 * |_  |
+		 *   | |
+		 *   |_|
+		 */
 		if(a%2==1)
 			for(int i=(a-1)/2;i<=h-b;i++)
 				sum+=(2*i+2-a);			
 		else
 			for(int i=a/2;i<=h-b;i++)
 				sum+=(2*(i-a/2+1));
-		
-		System.out.println(sum);
+//		System.out.println(sum);
+		out.println(sum);
+		out.close();
 		sc.close();		
 	}
 	
